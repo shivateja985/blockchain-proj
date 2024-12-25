1cde77db305fd0371a063c40956b59dfd3f94729
Shivateja <shivateja985@gmail.com>
2024-12-25 15:50:01 -0600

    updated test 1.1

--- test/Calculator.js
+++ test/Calculator.js
@@ -2,12 +2,15 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Calculator", () => {
+    let calculate
+    beforeEach(async () => {
+        const Calculator = await ethers.getContractFactory('Calculator')
+        calculate = await Calculator.deploy("add",3,2)
+    })
 
     it ("adds two numbers", async () => {
 
-        const Calculator = await ethers.getContractFactory('Calculator')
-        const calculate = await Calculator.deploy("add",3,2)
-        const result = await calculate.res()
-        expect(result).to.equal(5)
+        const result = await calculate.var1()
+        expect(result).to.equal(3)
     })
 })
